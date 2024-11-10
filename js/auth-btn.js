c19e1dca79ccc68e19f1a5681381e9600a4e9aba
Hong <o.kimhong168@gmail.com>
2024-11-10 22:01:33 +0700

    header

--- js/auth-btn.js
+++ js/auth-btn.js
@@ -50,3 +50,9 @@
                }
            })
    }
+
+   
+   window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
