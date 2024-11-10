5742dd80758c6d2ece315f29596edfc5e0386d79
Hong <o.kimhong168@gmail.com>
2024-11-10 21:59:04 +0700

    auth-btn

---
+++ js/auth-btn.js
@@ -0,0 +1,52 @@
+
+
+
+   // my script api 
+   console.log(sessionStorage.getItem('authToken'))
+   let authToken = sessionStorage.getItem('authToken')
+   let profiles = document.querySelector('#profiles');
+   let login = document.querySelector('#login');
+   let signup = document.querySelector('#signupForm')
+
+   if (!authToken) {
+       profiles.style.display = 'none';
+       login.style.display = 'inline'
+       signup.style.display = 'inline'
+   } else {
+       profiles.style.display = 'inline';
+       login.style.display = 'none'
+       signup.style.display = 'none'
+   }
+
+      // ===== change avatar 
+      let storedAvatar = sessionStorage.getItem('avatar');
+      if (storedAvatar) {
+          document.querySelectorAll('.changeImg').forEach(img => img.src = storedAvatar);
+      }
+
+
+   document.querySelector('#logout').addEventListener('click', () => {
+       logOut()
+   })
+   function logOut() {
+       fetch('https://mps4.chandalen.dev/api/logout', {
+           method: 'DELETE',
+           headers: {
+               'Accept': "application/json",
+               'Authorization': `Bearer ${authToken}`
+           }
+       })
+           .then(res => res.json())
+           .then(json => {
+               if (json.result === true) {
+                   sessionStorage.removeItem('authToken');
+                   console.log(json.message);
+                   alert(json.message)
+                   profiles.style.display = 'none';
+                   signup.style.display = 'inline'
+                   login.style.display = 'inline';
+               } else {
+                   alert(json.message);
+               }
+           })
+   }
